663c453e025e389269cbec2d70da7ffa37b0c681
izayaaaaaa <izaya.orihara0011@gmail.com>
2023-10-02 01:50:44 +0800

    fixeed vue config for prod

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/Izaya/' : '/',
-  outputDir: 'docs',
-  assetsDir: 'assets',
+  outputDir: 'dist',
   productionSourceMap: false,
   filenameHashing: true,
 };
