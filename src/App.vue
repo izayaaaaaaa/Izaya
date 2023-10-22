0d22ff13e8f56de023253d4ee59a6113e960411d
naarkz <naarkz@michie.dev>
2023-10-22 10:47:41 +0800

    feat: Add static nav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
-<!-- App.vue -->
+<script setup>
+import MainNavVue from './components/Navigation/MainNav.vue';
+</script>
+
 <template>
   <div id="app">
-    <router-view />
+    <div class="flex justify-center py-[67px] min-h-scree">
+      <div class="w-[35%]">
+        <header>
+          <MainNavVue />
+        </header>
+        <main>
+          <router-view />
+        </main>
+      </div>
+    </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 export default {
   content: ["./public/index.html", "./src/**/*.{js,vue}"],
   theme: {
+    colors: {
+      primary: "#373745",
+      secondary: "#454138",
+      light: "#FFFFFD",
+    },
     extend: {},
   },
   plugins: [],
