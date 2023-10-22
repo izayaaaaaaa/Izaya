b5467dea7fc69622080dfbe9df261e5e1eef4c68
naarkz <naarkz@michie.dev>
2023-10-22 13:06:14 +0800

    refactor: Componetize nav components

---
+++ src/components/Navigation/VerticalLine.vue
@@ -0,0 +1,9 @@
+<template>
+    <li>
+        <div :class="{ 'vertical-line': $store.state.mode == 'light', 'vertical-line-dark': $store.state.mode == 'dark' }">
+        </div>
+    </li>
+</template>
+
+<script>
+</script>
