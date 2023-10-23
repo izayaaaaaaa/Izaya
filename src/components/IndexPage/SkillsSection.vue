752e28df16c6f43c8f017abb1bd69085db05021e
naarkz <naarkz@michie.dev>
2023-10-24 07:28:35 +0800

    feat: Add skills section

--- src/components/IndexPage/SkillsSection.vue
+++ src/components/IndexPage/SkillsSection.vue
@@ -1,17 +1,38 @@
 <template>
     <div class="section-wrapper">
         <SectionLayout title="Skills">
-
+            <div class="flex gap-x-[27px]">
+                <div class="">
+                    <ul class="flex flex-col gap-y-[3px]">
+                        <li v-for="(key, index) in Object.keys(data)" :key="index" class="font-bold uppercase text-[11px]">
+                            {{ key }}
+                        </li>
+                    </ul>
+                </div>
+                <div class="">
+                    <ul class="flex flex-col gap-y-[3px]">
+                        <li v-for="(key, index) in Object.keys(data)" :key="index" class="text-xs font-normal">
+                            {{ data[key].join(', ') }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </SectionLayout>
     </div>
 </template>
 
 <script>
 import SectionLayout from "../Layouts/SectionLayout.vue"
+import skillsData from '../../data/skills.json';
 
 export default {
     components: {
         SectionLayout
+    },
+    data() {
+        return {
+            data: skillsData,
+        };
     },
 }
 </script>
